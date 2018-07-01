9ba9926d6a05f89a23b88e03466f157d1e628f90
jurcakmatej <jurcak.matej@gmail.com>
2018-07-02 01:21:56 +0200

    register/proxy

---
+++ accounts.py
@@ -0,0 +1,30 @@
+import random
+import string
+from random import choice
+from string import ascii_lowercase
+import requests
+from lxml.html import fromstring
+from itertools import cycle
+import traceback
+import proxxy
+
+
+def create_emails(urla, amount):
+    emails = []
+    url = urla + 'https://' + urla + '-en.ikariam.gameforge.com/index.php?action=newPlayer&function=createAvatar'
+    for i in range(0, amount):
+        part = ''.join(choice(ascii_lowercase) for i in range(7))
+        mail = '@gmail.com'
+        part += str(i)
+        email = part + mail
+        emails.append({'email': email, 'name': part})
+    proxies = proxxy.get_proxies()
+    proxy_pool = cycle(proxies)
+    for i in range(0, 20):
+        proxy = next(proxy_pool)
+        payload = {'email': emails[i]['email'], 'name': emails[i]['name'], 'agb': 'on', 'password': 'matejko123',
+                   'uni_url': urla + '-en.ikariam.gameforge.com'}
+        try:
+            response = requests.post(url, data=payload, proxies={"http": proxy, "https": proxy})
+        except:
+            print("Skipping. Connnection error")
---
+++ proxxy.py
@@ -0,0 +1,16 @@
+import requests
+from lxml.html import fromstring
+from itertools import cycle
+import traceback
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    # maximum 20 proxies
+    for i in parser.xpath('//tbody/tr')[:20]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
