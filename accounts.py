13f0527a6fa7f9fced7857f1570d61c49e125429
jurcakmatej <jurcak.matej@gmail.com>
2018-07-02 12:48:23 +0200

    invite friends

--- accounts.py
+++ accounts.py
@@ -8,23 +8,50 @@ from itertools import cycle
 import traceback
 import proxxy
 
-
-def create_accounts(urla, amount):
+def create_emails(amount):
     emails = []
-    url = urla + 'https://' + urla + '-en.ikariam.gameforge.com/index.php?action=newPlayer&function=createAvatar'
     for i in range(0, amount):
         part = ''.join(choice(ascii_lowercase) for i in range(7))
         mail = '@gmail.com'
         part += str(i)
         email = part + mail
         emails.append({'email': email, 'name': part})
+    return emails
+
+def create_accounts(urla, amount):
+    emails = create_emails(amount)
+    url = 'https://' + urla + '-en.ikariam.gameforge.com/index.php?action=newPlayer&function=createAvatar'
     proxies = proxxy.get_proxies()
     proxy_pool = cycle(proxies)
-    for i in range(0, 20):
+    for i in range(0, amount):
         proxy = next(proxy_pool)
         payload = {'email': emails[i]['email'], 'name': emails[i]['name'], 'agb': 'on', 'password': 'matejko123',
                    'uni_url': urla + '-en.ikariam.gameforge.com'}
         try:
             response = requests.post(url, data=payload, proxies={"http": proxy, "https": proxy})
+            print (emails[i]['email'], emails[i]['name'])
+        except:
+            print("Skipping. Connnection error")
+
+def invite_friends(fh, playerId):
+    url = 'https://s35-en.ikariam.gameforge.com/index.php'
+    proxies = proxxy.get_proxies()
+    emails = create_emails(10)
+    proxy_pool = cycle(proxies)
+    for i in range(0, 10):
+        proxy = next(proxy_pool)
+        payload = {
+            'action': 'newPlayer',
+            'friendId': playerId, #'29969',
+            'fh': fh, #'4f158c4f36d1dfbf022ee3b08ec69d82',
+            'function': 'createAvatar',
+            'email': emails[i]['email'],
+            'name': emails[i]['name'],
+            'agb': 'on',
+            'password': 'matejko123'
+        }
+        try:
+            response = requests.post(url, data=payload, proxies={"http": proxy, "https": proxy})
+            print(emails[i]['emails'], emails[i]['name'])
         except:
             print("Skipping. Connnection error")
--- proxxy.py
+++ proxxy.py
@@ -10,7 +10,11 @@ def get_proxies():
     proxies = set()
     # maximum 20 proxies
     for i in parser.xpath('//tbody/tr')[:20]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
-            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            proxies.add(proxy)
+        # if i.xpath('.//td[7][contains(text(),"yes")]'):
+        proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+        proxies.add(proxy)
     return proxies
+
+def get_proxy():
+    proxies = get_proxies()
+    return next(proxies)
