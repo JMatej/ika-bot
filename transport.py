c2736d37fe2ad0597dd0cc70338c19d92786f99c
Matej Sladek <matejsladek10@gmail.com>
2018-07-03 18:14:33 +0200

    fix

--- transport.py
+++ transport.py
@@ -1,3 +1,6 @@
+import time
+import traceback
+
 from botapi import BotAPI
 
 world = "s35-en.ikariam.gameforge.com"
@@ -5,38 +8,35 @@ url = "https://s35-en.ikariam.gameforge.com"
 pwd = "matejko123"
 
 
-def get_groups():
-    groups = {}
-    lines = [line.rstrip() for line in open('groups.txt')]
-    master = lines[0].split(" ")[0]
-    master_city_id = -1
-    for idx, line in enumerate(lines):
-        print(idx)
-        act_email = line.split(" ")[0]
-        act_group_id = int(line.split(" ")[1])
-        bot = BotAPI(url, world, act_email, pwd)
-        state = bot.get_state()
-        city_id = state['city']['id']
-        if idx == 0:
-            master_city_id = city_id
-        if act_group_id not in groups:
-            groups[act_group_id] = [{'email': master, 'city_id': master_city_id}]
-        groups[act_group_id].append({'email': act_email, 'city_id': city_id})
-    return groups
+class TransportLuxury:
+    def __init__(self):
+        self.groups = self.get_groups()
+        self.lines = [line.rstrip() for line in open('groups.txt')]
 
+    def get_groups(self):
+        groups = {}
+        master = self.lines[0].split(" ")[0]
+        master_city_id = -1
+        for idx, line in enumerate(self.lines):
+            print(idx)
+            act_email = line.split(" ")[0]
+            act_group_id = int(line.split(" ")[1])
+            bot = BotAPI(url, world, act_email, pwd)
+            state = bot.get_state()
+            city_id = state['city']['id']
+            if idx == 0:
+                master_city_id = city_id
+            if act_group_id not in groups:
+                groups[act_group_id] = [{'email': master, 'city_id': master_city_id}]
+            groups[act_group_id].append({'email': act_email, 'city_id': city_id})
+        return groups
 
-def transport_luxury():
-    groups = get_groups()
-    lines = [line.rstrip() for line in open('groups.txt')]
-    for idx, line in enumerate(lines):
-        print(idx)
-        act_email = line.split(" ")[0]
-        act_group_id = int(line.split(" ")[1])
-        bot = BotAPI(url, world, act_email, pwd)
+    def transport_from(self, email, group_id):
+        bot = BotAPI(url, world, email, pwd)
         state = bot.get_state()
         luxury = state['town_hall_info']['luxury']
-        for partner in groups[act_group_id]:
-            if partner['email'] != act_email:
+        for partner in self.groups[group_id]:
+            if partner['email'] != email:
                 marble = 0
                 glass = 0
                 sulfur = 0
@@ -52,5 +52,21 @@ def transport_luxury():
                 # destination_city_id, wood, wine, marble, glass, sulfur
                 bot.transport(partner['city_id'], 0, wine, marble, glass, sulfur)
 
+    def transport(self):
+        for idx, line in enumerate(self.lines):
+            print(idx)
+            email = line.split(" ")[0]
+            group_id = int(line.split(" ")[1])
+            start = time.time()
+            print('account:', idx, email, group_id)
+            try:
+                self.transport_from(email, group_id)
+            except Exception as e:
+                print("type error: " + str(e))
+                print(traceback.format_exc())
+            end = time.time()
+            print('time: ', end - start)
 
-transport_luxury()
+
+inst = TransportLuxury()
+inst.transport()
